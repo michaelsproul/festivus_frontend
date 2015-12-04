841d43692a832c988ac4dd7f3c05d15b5faba417
Michael Sproul <micsproul@gmail.com>
2015-12-04 11:02:05 +1100

    Mucking around with solar and total power curves.

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ var URI = require("urijs");
 var Plotly = require("plotly.js");
 var jQuery = require("jquery");
 
-var day_start = moment({day: 2, hour: 0, minute: 0, second: 0});
+var day_start = moment({hour: 0, minute: 0, second: 0});
 var now = moment();
 
 var url = URI("http://localhost:3000/power").query({
@@ -16,16 +16,26 @@ console.log("Requesting data from:", url);
 jQuery.getJSON(url).done(function(raw_data) {
     console.log("SUCCESS");
     // Plot
-    var data = {
+    var data = [
+    {
         type: "scatter",
         name: "total_power",
         line: {shape: "hv"},
-        fill: "tozeroy",
-        fillcolor: "#93BDBB",
-        x: raw_data.x,
-        y: raw_data.y
-    };
-    Plotly.plot("power_chart", [data], { margin: { pad: 2 } });
+        //fill: "tozeroy",
+        //fillcolor: "#93BDBB",
+        x: raw_data.total.x,
+        y: raw_data.total.y
+    },
+    {
+        type: "scatter",
+        name: "solar_power",
+        line: {shape: "hv"},
+        //fill: "tozeroy",
+        //fillcolor: "##62C46E",
+        x: raw_data.solar.x,
+        y: raw_data.solar.y
+    }];
+    Plotly.plot("power_chart", data, { margin: { pad: 2 } });
 
 }).error(function() {
     console.log("ERROR");
